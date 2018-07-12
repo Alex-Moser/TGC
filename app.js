6d0d72195bbe0f19d3c467637ffd8ebdce242fcb
Alex <alexandermoser1@aol.com>
2018-07-11 20:26:02 -0500

    mlab db stuff

--- app.js
+++ app.js
@@ -25,7 +25,12 @@ var commentRoutes   = require('./routes/comments'),
 
 // Conect to Post_yep db (database), if no db, creates one (i.e. the first time)
 // mongoose.connect('mongodb://localhost/tgc');
-mongoose.connect('mongodb://alexandertmoser:god727@ds235311-a0.mlab.com:35311,ds235311-a1.mlab.com:35311/tgc?replicaSet=rs-ds235311');
+mongoose.connect('mongodb://alexandertmoser:god727@ds235311-a0.mlab.com:35311,ds235311-a1.mlab.com:35311/tgc?replicaSet=rs-ds235311', {
+  socketTimeoutMS: 0,
+  keepAlive: true,
+  reconnectTries: 30
+});
+
 
 // Get Mongoose to use the global promise library - TODO:async callback cleanup.
 mongoose.Promise = global.Promise;
