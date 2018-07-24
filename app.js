b4180d8f6b2b73e0b99d04f055057fb5ff811f96
Alex <alexandermoser1@aol.com>
2018-07-24 14:15:38 -0500

    landing page and some index

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ var commentRoutes   = require('./routes/comments'),
     messageRoutes   = require('./routes/messages')
 
 // Conect to Post_yep db (database), if no db, creates one (i.e. the first time)
-mongoose.connect('mongodb://localhost/tgc');
+// mongoose.connect('mongodb://localhost/tgc');
 var uri = 'mongodb://alexandertmoser:god727@ds235311-a0.mlab.com:35311,ds235311-a1.mlab.com:35311/tgc?replicaSet=rs-ds235311';
 
 
-// mongoose.connect(uri, {
-//   socketTimeoutMS: 0,
-//   keepAlive: true,
-//   reconnectTries: 30
-// });
+mongoose.connect(uri, {
+  socketTimeoutMS: 0,
+  keepAlive: true,
+  reconnectTries: 30
+});
 
 
 // Get Mongoose to use the global promise library - TODO:async callback cleanup.
@@ -79,7 +79,7 @@ app.use(postRoutes);
 app.use(messageRoutes);
 
 // Listens on local server and notes this in terminal.
-app.listen(3000, () => console.log('Server has started.'));
+// app.listen(3000, () => console.log('Server has started.'));
 
 // Listens on Heroku server
-// app.listen(process.env.PORT, '0.0.0.0');
+app.listen(process.env.PORT, '0.0.0.0');
