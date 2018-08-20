4de69d373959fba80a54223dddf4f7b9c998e71d
Alex <alexandermoser1@aol.com>
2018-08-20 16:26:48 -0500

    Finished basics regarding macro-scaling aspects of the index page

--- public/customjQuery/slidebar.js
+++ public/customjQuery/slidebar.js
@@ -1,80 +1,104 @@
-$( "#hideSidebar" ).click(function() {
+$(document).ready(function() {
+    // When the HIDE button is clicked...
+    $( "#hideSidebar" ).click(function() {
 
-    // If Sidebar is not hidden
-    if ($("div.sidebar").hasClass("show-sidebar")) {
+        // If Sidebar is not hidden
+        if ($("div.sidebar").hasClass("show-sidebar")) {
 
-        /* Shrink sidebar to left */
-        $( "div.sidebar" ).animate({
-          width: "3em"
-      }, 500, "swing", function() {
-          // Animation complete.
-      });
+        // HIDE SIDEBAR FUNCTIONS---------------------
+            // Shrink sidebar to left
+            $( "div.sidebar" ).animate({
+              width: "3em"
+          }, 500, "swing", function() {
+              // Animation complete.
+          });
 
-      // slide logo and filter options
-      $("div.logo").animate({
-          marginLeft: "-15em"
-      }, 500, "swing", function() {
-          // Animation complete
-      });
+          // slide logo and filter options
+          $("div.logo").animate({
+              marginLeft: "-15em"
+          }, 500, "swing", function() {
+              // Animation complete
+          });
 
-      // Fade text on sidebar buttons
-      $("span.sidebar-btn-text").animate({
-          opacity: "toggle"
-      }, 200, "swing", function() {
-          // Now toggle on opacity of right arrow icon
-         $("span.sidebar-btn-icon > i.fa-angle-right").animate({
-             opacity: "1"
-         }, 200, "swing", function() {
-             // Animation Complete
+          // Fade text on sidebar buttons
+          $("span.sidebar-btn-text").animate({
+              opacity: "toggle"
+          }, 200, "swing", function() {
+              // Now toggle on opacity of right arrow icon
+             $("span.sidebar-btn-icon > i.fa-angle-right").animate({
+                 opacity: "1"
+             }, 200, "swing", function() {
+                 // Animation Complete
+             });
          });
-     });
-      // Move rest of page to the left
-      $(".post-index-c").animate({
-          paddingLeft: "3em"
-      }, 500, "swing", function() {
-          // Animation complete
-      });
+          // Move rest of page to the left
+          $(".post-index-c").animate({
+              paddingLeft: "0"
+          }, 500, "swing", function() {
+              // Animation complete
+          });
 
-  } else {
+      } else { // If the Sidebar is hidden...
 
-      /* Shrink sidebar to left */
-      $( "div.sidebar" ).animate({
-        width: "14em"
-    }, 500, "swing", function() {
-        // Animation complete.
+          // SHOW SIDEBAR FUNCTIONS-----------------------------------
+          /* Shrink sidebar to left */
+          $( "div.sidebar" ).animate({
+            width: "14em"
+        }, 500, "swing", function() {
+            // Animation complete.
 
-    });
+        });
 
-    // Fade Logo and "Filter options"
-    $("div.logo").animate({
-        marginLeft: "0"
-    }, 500, "swing", function() {
-        // Animation complete
-    });
+        // Fade Logo and "Filter options"
+        $("div.logo").animate({
+            marginLeft: "0"
+        }, 500, "swing", function() {
+            // Animation complete
+        });
 
-    // Now toggle on opacity of right arrow icon
-   $("span.sidebar-btn-icon > i.fa-angle-right").animate({
-       opacity: "0"
-   }, 200, "swing", function() {
-       // Fade text on sidebar buttons
-       $("span.sidebar-btn-text").animate({
-           opacity: "toggle"
+        // Now toggle on opacity of right arrow icon
+       $("span.sidebar-btn-icon > i.fa-angle-right").animate({
+           opacity: "0"
        }, 200, "swing", function() {
+           // Fade text on sidebar buttons
+           $("span.sidebar-btn-text").animate({
+               opacity: "toggle"
+           }, 200, "swing", function() {
 
-      });
+          });
+       });
+
+        // Move rest of page to the left
+        $(".post-index-c").animate({
+            paddingLeft: "14em"
+        }, 500, "swing", function() {
+            // Animation complete
+
+        });
+
+
+      }
+
+
+        $("div.sidebar").toggleClass("show-sidebar");
+
    });
 
-    // Move rest of page to the left
-    $(".post-index-c").animate({
-        paddingLeft: "14em"
-    }, 500, "swing", function() {
-        // Animation complete
-    });
+   // Event listener for window resize.
+   $( window ).resize(function(){
+      if ($( window ).width() < 756){
+
+          $(".post-index-c").css("paddingLeft", "0");
+      } else if ($( window ).width() >= 756){
+          if ($("div.sidebar").hasClass("show-sidebar")){
+             $(".post-index-c").css("paddingLeft", "14em");
+         } else {
+             $(".post-index-c").css("paddingLeft", "3em");
+         }
+
+      }
+   });
 
 
-  }
-
-
-    $("div.sidebar").toggleClass("show-sidebar");
 
 });
